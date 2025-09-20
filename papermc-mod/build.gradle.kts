d30b5bcf915544ad88147fe52de4d95fac0c2d7a
Domas Leščinskas <domas.lescinskas@gmail.com>
2025-09-20 16:30:43 +0300

    Initialized the SkyLink Java mod's codebase

--- papermc-mod/build.gradle.kts
+++ papermc-mod/build.gradle.kts
@@ -1,1 +1,48 @@
-// Empty build.gradle.kts file
+plugins {
+    java
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
+    id("com.gradleup.shadow") version "9.1.0"
+}
+
+group = "skylink"
+version = "1.0.0-beta"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    paperweight.paperDevBundle("1.21.1-R0.1-SNAPSHOT")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.mikesamuel:json-sanitizer:1.2.3")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+
+    sourceCompatibility = JavaVersion.VERSION_21.toString()
+    targetCompatibility = JavaVersion.VERSION_21.toString()
+}
+
+tasks.shadowJar {
+    archiveFileName.set("skylink-${version}.jar")
+
+    relocate("okhttp3", "skylink.libs.okhttp3")
+    relocate("com.google.gson", "skylink.libs.gson")
+    relocate("com.mikesamuel", "skylink.libs.mikesamuel")
+
+    minimize()
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
