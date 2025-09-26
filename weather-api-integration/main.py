626b8195633d44ba2474bfbbfe9072e0038f0b64
Dominykas Vaitkūnas <dominykasvaitkunas09@gmail.com>
2025-09-26 09:55:03 +0000

    Pridėta orų prognozės API integracija į main.py failą

--- weather-api-integration/main.py
+++ weather-api-integration/main.py
@@ -1,1 +1,15 @@
-# Empty main.py file
+import requests
+
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+    "latitude": 54.6892,
+    "longitude": 25.2798,
+    "current": "is_day,temperature_2m,rain",
+}
+
+response = requests.get(url, params=params)
+
+data = response.json()
+print("Temperatūra:", data["current"]["temperature_2m"], "°C")
+print("Ar diena:", "Taip" if data["current"]["is_day"] == 1 else "Ne")
+print("Ar lyja:", "Taip" if data["current"]["rain"] > 0 else "Ne")
