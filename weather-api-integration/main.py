37d0480c01494fe15235d316047a81f8bfb8aadc
Dominykas Vaitkūnas <dominykasvaitkunas09@gmail.com>
2025-10-07 08:37:10 +0000

    Refactor weather data retrieval functions and update city input handling

--- weather-api-integration/main.py
+++ weather-api-integration/main.py
@@ -1,15 +1,35 @@
 import requests
 
-url = "https://api.open-meteo.com/v1/forecast"
-params = {
-    "latitude": 54.6892,
-    "longitude": 25.2798,
-    "current": "is_day,temperature_2m,rain",
-}
+city = 'vilnius' # Laikinas placeholderis, kol ateityje gausiu miesto inputą iš Minecraft
 
-response = requests.get(url, params=params)
+# Funkcija, kuri grąžina miesto platumą ir ilgumą
+def get_lat_lon(city):
+    url = 'https://nominatim.openstreetmap.org/search'
+    params = {
+        'q': city,
+        'format': 'json',
+        'limit': 1,
+    }
 
-data = response.json()
-print("Temperatūra:", data["current"]["temperature_2m"], "°C")
-print("Ar diena:", "Taip" if data["current"]["is_day"] == 1 else "Ne")
-print("Ar lyja:", "Taip" if data["current"]["rain"] > 0 else "Ne")
+    response = requests.get(url, params=params, headers={'User-Agent': 'city-lat-lon-script'})
+    data = response.json()
+
+    lat = data[0]['lat']
+    lon = data[0]['lon']
+    return lat, lon
+
+# Funkcija, kuri grąžina miesto temperatūrą
+def get_temperature():
+    url = 'https://api.open-meteo.com/v1/forecast'
+    params = {
+        'latitude': get_lat_lon(city)[0],
+        'longitude': get_lat_lon(city)[1],
+        'current': 'temperature_2m',
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    return data['current']['temperature_2m']
+
+print(get_lat_lon('Uk/London'))
